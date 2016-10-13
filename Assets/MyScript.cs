2ad1abbc794a885996056bd47eeb52412edba071
Craig Bremer Admin <cbremeradmin@arp.local>
2016-10-13 13:06:59 -0600

    Added new lines to MyScript

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -6,6 +6,7 @@ public class MyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("you found the sausage");
+        print("you're a wanker");
 	}
 	
 	// Update is called once per frame
